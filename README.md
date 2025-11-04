be4b0bd7a39d561e3bd624872dc7f828432d0ead
Gibson Lee <gibsonlee08@gmail.com>
2025-11-03 20:42:38 -0600

    Add clock.py script for future use, update repo name and readme

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# PicoLaunchTimer
-![GitHub Release](https://img.shields.io/github/v/release/gsl4295/PicoLaunchTimer?include_prereleases&sort=date&display_name=tag)
-![GitHub commit activity](https://img.shields.io/github/commit-activity/t/gsl4295/PicoLaunchTimer)
-![GitHub Issues or Pull Requests](https://img.shields.io/github/issues/gsl4295/PicoLaunchTimer)<br>
+# PicoTimers
+![GitHub Release](https://img.shields.io/github/v/release/gsl4295/PicoTimers?include_prereleases&sort=date&display_name=tag)
+![GitHub commit activity](https://img.shields.io/github/commit-activity/t/gsl4295/PicoTimers)
+![GitHub Issues or Pull Requests](https://img.shields.io/github/issues/gsl4295/PicoTimers)<br>
 A CircuitPython-based microcontroller dedicated to showing a live countdown to the next orbital rocket launch.
 
 ### Hardware
-- The pin diagram for my screen specifically is located in `/info/pin-info.md`.
+- The pin diagram for my screen specifically is located in `/info/pin-info.md`
 - A top-down view of my setup is in `/info/pico-top-down-view.jpg`
-- I used a [Raspberry Pi WH](https://www.amazon.com/Raspberry-Pi-RP-PICO-WH-Pico-WH/dp/B0C58X9Q77) (wifi enabled, headers attached)
+- I used a [Raspberry Pi WH](https://www.amazon.com/Raspberry-Pi-RP-PICO-WH-Pico-WH/dp/B0C58X9Q77) (**W**ifi enabled, **H**eaders attached)
 - The exact screen I used is a 1.8" 128x160 SPI TFT display. Find it [here on Amazon](https://a.co/d/0OCU4uG).
 
 ### Installation & Setup
@@ -30,7 +30,7 @@ WIFI = "placeholder"
 PASS = "placeholder"
 ```
 
-### Features
+### launch.py
 - *Manual countdown data*
   - Toggles the screen between the next orbital rocket and a hardcoded date & time input from the user.
   - This feature can be toggled with a button. More details are located in `pin-info.md`
@@ -39,10 +39,19 @@ PASS = "placeholder"
   - The `adafruit_display_text` library is simply amazing for this purpose, as you'll especially see from the scrolling text.
 - *[RocketLaunch.Live](https://rocketlaunch.live) integration*
   - This code partially relies on data from [the rocketlaunch.live API](https://rocketlaunch.live/api).
-  - It's like the Wikipedia of launch tracking - anyone can contribute.
+  - It's like the Wikipedia of launch tracking, so anyone can contribute.
   - Plus, more contributions will only make this code more reliable!
 - *Automatic DST Conversion*
   - It calls [timeapi.io](https://timeapi.io) in order to update the daylight savings time calculations at startup only.
 
-### Footnotes
-Thank you for reading! More to come.
+### clock.py
+- A simple digital clock that displays the current time and date on the screen.
+- Rather than using timeapi.io, this code uses the built-in RTC (real-time clock).
+- Right now it's just meant for desk use, so it's ultra-simple.
+
+### Future Improvements
+- Main "picker" script to choose between launch.py and clock.py at startup.
+- More time-related subtitles in clock.py.
+- Try to use RTC for launch.py to avoid API calls for time.
+
+Thanks for reading :)
---
+++ clock.py
@@ -0,0 +1,178 @@
+try:
+    from board_definitions.raspberry_pi_pico_w import GP10, GP11, GP16, GP17, GP18, GP0, LED
+except ImportError:  # pragma: no cover
+    # noinspection PyPackageRequirements
+    from board import GP10, GP11, GP16, GP17, GP18, GP0, LED
+from busio import SPI
+from digitalio import DigitalInOut, Direction, Pull
+import gc
+
+from displayio import release_displays, Group, Bitmap, Palette, TileGrid
+from terminalio import FONT
+from fourwire import FourWire
+from adafruit_display_text import label
+from adafruit_display_text.scrolling_label import ScrollingLabel
+from adafruit_st7735r import ST7735R
+
+# noinspection PyPackageRequirements
+from wifi import radio
+# noinspection PyPackageRequirements
+import socketpool
+import ssl
+from os import getenv
+
+import adafruit_requests
+import adafruit_ntp
+from adafruit_datetime import datetime, timedelta
+from time import sleep
+import time, rtc
+
+
+class PicoControl:
+    def __init__(self):
+        self.counter = 0
+        mosi_pin = GP11
+        clk_pin = GP10
+        reset_pin = GP17
+        cs_pin = GP18
+        dc_pin = GP16
+        release_displays()
+        spi = SPI(clock=clk_pin, MOSI=mosi_pin)
+        display_bus = FourWire(spi, command=dc_pin, chip_select=cs_pin, reset=reset_pin)
+        self.display = ST7735R(display_bus, width=128, height=160, bgr=True)
+        self.led = DigitalInOut(LED)
+        self.led.direction = Direction.OUTPUT
+        self.pool = socketpool.SocketPool(radio)
+        # noinspection PyTypeChecker
+        self.requests = adafruit_requests.Session(self.pool, ssl.create_default_context())
+        self.launch: dict = {}
+        self.name: str = ""
+        self.t0: str = ""
+        self.win_open: str = ""
+        self.win_close: str = ""
+        self.provider: str = ""
+        self.vehicle: str = ""
+        self.pad: str = ""
+        self.lc: str = ""
+        self.country: str = ""
+        self.y: str = ""
+        self.m: str = ""
+        self.d: str = ""
+        self.h: str = ""
+        self.mi: str = ""
+        self.dy: str = ""
+        self.button = DigitalInOut(GP0)
+        self.button.direction = Direction.INPUT
+        self.button.pull = Pull.DOWN
+        self.placeholder_setting: bool = False
+        self.utc_delta: int = 0
+
+    def led_toggle(self, toggle: bool):
+        self.led.value = toggle
+
+    # noinspection PyUnresolvedReferences
+    @staticmethod
+    def manage_memory(verbose=False):
+        if verbose:
+            old_memory_available = gc.mem_free()
+            gc.collect()
+            print(f"Memory cleaned: {old_memory_available} -> {gc.mem_free()} bytes free")
+        else:
+            gc.collect()
+
+    def visuals(self, accent: tuple):
+        # Add background with accent color
+        splash = Group()
+        self.display.root_group = splash
+        color_bitmap = Bitmap(128, 160, 1)
+        color_palette = Palette(1)
+        color_palette[0] = (0, 0, 0)
+        bg_sprite = TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+        splash.append(bg_sprite)
+
+        countdown_text_group = Group(scale=3, x=20, y=30)
+        self.countdown_text_area = label.Label(FONT, text="00:00", color=accent)
+        countdown_text_group.append(self.countdown_text_area)
+        splash.append(countdown_text_group)
+
+        main_text_group = Group(scale=1, x=20, y=65)
+        self.main_row_1 = label.Label(FONT, y=0, text="", color=accent)
+        self.main_row_2 = label.Label(FONT, y=15, text="", color=accent)
+        self.main_row_3 = label.Label(FONT, y=30, text="", color=accent)
+        self.main_row_4 = label.Label(FONT, y=45, text="Pico Clock", color=accent)
+        self.main_row_5 = label.Label(FONT, y=60, text="by gsl4295", color=accent)
+        self.main_row_6 = label.Label(FONT, y=75, text="Loading...", color=accent)
+        main_text_group.append(self.main_row_1)
+        main_text_group.append(self.main_row_2)
+        main_text_group.append(self.main_row_3)
+        main_text_group.append(self.main_row_4)
+        main_text_group.append(self.main_row_5)
+        main_text_group.append(self.main_row_6)
+        splash.append(main_text_group)
+
+        print(f"Screen rendered with an accent of RGB value {accent}")
+
+    @staticmethod
+    def wifi_connect():
+        while True:
+            try:
+                radio.connect(getenv("WIFI"), getenv("PASS"))
+                print(f"Connected to the Internet via {getenv("WIFI")}")
+                return
+            except ConnectionError:
+                print("No connection, trying again in 2 seconds")
+                sleep(2)
+                continue
+
+    def get_time(self) -> int:
+        # for startup alone when pico clock is not synced
+        ntp = adafruit_ntp.NTP(self.pool, server="pool.ntp.org", tz_offset=-6)
+        rtc.RTC().datetime = ntp.datetime
+
+        self.years = int(rtc.RTC().datetime.tm_year)
+        self.months = int(rtc.RTC().datetime.tm_mon)
+        self.days = int(rtc.RTC().datetime.tm_mday)
+        self.hours = int(rtc.RTC().datetime.tm_hour)
+
+        if self.hours > 12:
+            self.hours -= 12
+
+        self.minutes = int(rtc.RTC().datetime.tm_min)
+        self.seconds = int(rtc.RTC().datetime.tm_sec)
+        return
+
+    def countdown_loop(self, update_time=15, display_interval=15):
+        # http_time defines the time (seconds) that it takes for countdown_loop() to break out of the loop.
+        # display_interval defines the time (seconds) that the display has before refreshing.
+
+        num_cycles = int(update_time / display_interval)
+
+        for cycle in range(num_cycles):
+            self.countdown_text_area.text = f"{self.hours:02}:{self.minutes:02}"
+            self.main_row_1.text = f"{self.months}/{self.days}/{self.years}"
+            self.main_row_2.text = f""
+            self.main_row_3.text = f""
+            self.main_row_4.text = f""
+            self.main_row_5.text = f""
+            self.main_row_6.text = f""
+
+            self.counter += 1
+            if self.counter == 1:
+                print(f"Countdown active")
+
+            sleep(display_interval)
+
+    def run_loop(self, r=255, g=255, b=255):
+        self.led_toggle(False)
+        self.visuals((r, g, b))
+        self.wifi_connect()
+        while True:
+            self.get_time()
+            self.countdown_loop()
+            self.manage_memory(verbose=False)
+
+
+if __name__ == "__main__":
+    print("System on internal power")
+    control = PicoControl()
+    control.run_loop(r=0, b=0)
